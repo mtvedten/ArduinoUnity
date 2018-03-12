a93ef88085c9e763964c937b5e4fb6c0c1fe4ab5
mtvedten <morten@mediafarm.no>
2018-03-12 01:06:13 +0100

    Test of Serial communication
    Connecting Unity to Arduino using serial port.
    To do: Dynaically find serial ports. Not hard coded.
    
    To varianter: En enkel skervet av meg, fra scratch.
    og en en som bruker et libray lastet ned fra nettet. Denn er nok mer
    robust. Håndterer at kobling trekkes ut og sette i igjen.

---
+++ Assets/MedBrukAvSerialLib/scripts/HandleArduinoInput.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleArduinoInput : MonoBehaviour {
+
+    public SerialController serialController;
+
+    // Initialization
+    void Start()
+    {
+        serialController = GameObject.Find("SerialController").GetComponent<SerialController>();
+
+        Debug.Log("Press A or Z to execute some actions");
+    }
+
+    // Executed each frame
+    void Update()
+    {
+        //---------------------------------------------------------------------
+        // Send data
+        //---------------------------------------------------------------------
+
+        // If you press one of these keys send it to the serial device. A
+        // sample serial device that accepts this input is given in the README.
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Sending A");
+            serialController.SendSerialMessage("A");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Debug.Log("Sending Z");
+            serialController.SendSerialMessage("Z");
+        }
+
+
+        //---------------------------------------------------------------------
+        // Receive data
+        //---------------------------------------------------------------------
+
+        string message = serialController.ReadSerialMessage();
+
+        if (message == null)
+            return;
+
+        // Check if the message is plain data or a connect/disconnect event.
+        if (ReferenceEquals(message, SerialController.SERIAL_DEVICE_CONNECTED))
+            Debug.Log("Connection established");
+        else if (ReferenceEquals(message, SerialController.SERIAL_DEVICE_DISCONNECTED))
+            Debug.Log("Connection attempt failed or disconnection detected");
+        else
+            Debug.Log("Message arrived: " + message);
+
+
+       
+      
+        
+
+            switch (message)
+            {
+                case "0":
+                    SetCubeColor(Color.white);
+                    break;
+                case "1":
+                    SetCubeColor(Color.blue);
+                    break;
+                case "2":
+                    SetCubeColor(Color.yellow);
+                    break;
+                case "3":
+                    SetCubeColor(Color.red);
+                    break;
+                case "4":
+                    SetCubeColor(Color.green);
+                    break;
+                case "5":
+                    SetCubeColor(Color.black);
+                    break;
+                case "6":
+                startRotation(new Vector3(-1, -3, -4));
+                break;
+                case "7":
+     
+                    startRotation(new Vector3(2,2,2));
+                break;
+                default:
+
+                    break;
+            }
+
+
+
+
+
+    }
+
+    void SetCubeColor(Color newCol)
+    {
+
+        Renderer rend = GetComponent<Renderer>();
+        rend.material.color = newCol;
+    }
+
+
+   void startRotation(Vector3 turn)
+    {
+        gameObject.GetComponent<Rigidbody>().AddTorque(turn);
+       
+
+    }
+
+
+
+
+}
---
+++ Assets/MortensEgenSerial/Scripts/SerialMoveCube.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+// Mortens hardcoded serial 
+
+public class SerialMoveCube : MonoBehaviour {
+
+  static string portName = "COM4";
+  static int baudRate = 9600;
+
+
+    SerialPort sp = new SerialPort(portName, baudRate);
+
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Starting");
+
+
+        // open serial con.
+        sp.Open();
+        sp.ReadTimeout = 1;
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+        if (sp.IsOpen)
+        {
+          
+
+            try
+            {
+               
+                string serialInput = sp.ReadLine();
+                print(serialInput);
+
+                switch (serialInput)
+                {
+                    case "0":
+                        SetCubeColor(Color.white);
+                        break;
+                    case "1":
+                        SetCubeColor(Color.blue);
+                        break;
+                    case "2":
+                        SetCubeColor(Color.yellow);
+                        break;
+                    case "3":
+                        SetCubeColor(Color.red);
+                        break;
+                    case "4":
+                        SetCubeColor(Color.green);
+                        break;
+                    case "5":
+                        SetCubeColor(Color.black);
+                        break;
+                    case "6":
+                        SetCubeColor(Color.grey);
+                        break;
+                    case "7":
+                        SetCubeColor(Color.magenta);
+                        break;
+                    default:
+                       
+                        break;
+                }
+
+            }
+            catch
+            {
+               
+
+            }
+        }
+        else
+        {
+            Debug.Log("Serial Disconnected");
+
+
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+           Debug.Log("Sending; A");
+           sp.Write("A");
+           
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Debug.Log("Sending Z");
+            sp.Write("Z");
+            //serialController.SendSerialMessage("Z");
+        }
+    }
+
+    void SetCubeColor(Color newCol)
+    {
+
+        Renderer rend = GetComponent<Renderer>();
+        rend.material.color = newCol;
+    }
+
+    void OnApplicationQuit()
+    {
+        Debug.Log("Exiting...");
+        sp.Close();
+    }
+}
